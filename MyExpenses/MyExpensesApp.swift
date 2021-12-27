84ca4518d01d0db7dd1a5511e2d09b74a18f8e86
Nguyen Tuong Quy <tuongquynguyen.159@gmail.com>
2021-12-27 20:27:20 +0700

    Initial Commit

---
+++ MyExpenses/MyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesApp.swift
+//  MyExpenses
+//
+//  Created by KET on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
